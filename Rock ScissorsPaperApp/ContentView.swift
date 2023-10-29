26cb0d60e30342e10c2049c9c38403b4ccde5b63
polosa00 <i@apolochanin.ru>
2023-10-29 16:27:58 +0300

    Initial Commit

---
+++ Rock ScissorsPaperApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rock ScissorsPaperApp
+//
+//  Created by Александр Полочанин on 29.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rock ScissorsPaperApp/Rock_ScissorsPaperAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_ScissorsPaperAppApp.swift
+//  Rock ScissorsPaperApp
+//
+//  Created by Александр Полочанин on 29.10.23.
+//
+
+import SwiftUI
+
+@main
+struct Rock_ScissorsPaperAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
